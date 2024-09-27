eac371db05ad2487e16a155df0b78963b23fab43
hungho3101 <119468273+hungho3101@users.noreply.github.com>
2024-09-28 01:31:01 +0700

    Hello world

--- src/server.js
+++ src/server.js
@@ -1,34 +1,11 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express')
+const app = express()
+const port = 3000
 
-const routes = require("./routes");
-const { connectionDatabase } = require("./config/database");
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const app = express();
-
-const hostname = process.env.HOSTNAME;
-const port = process.env.PORT || 8081;
-
-// Middleware: Enable CORS for specified origin
-let corsOptions = {
-  origin: "*",
-};
-
-app.use(cors(corsOptions));
-
-// Middleware: Parse requests with JSON payload
-app.use(express.json());
-
-// Middleware: Parse requests with x-www-form-urlencoded content type
-app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.use('/api/v1', routes);
-
-connectionDatabase();
-
-// Start the server
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
